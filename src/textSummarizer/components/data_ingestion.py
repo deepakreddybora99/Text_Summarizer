196e7b83a1900b8bf7dffdfa5b002bb627052bff
deepakreddybora99 <deepakreddybora@gmail.com>
2025-04-19 15:37:32 +0530

    Data Ingestion Added

---
+++ src/textSummarizer/components/data_ingestion.py
@@ -0,0 +1,36 @@
+#component
+import os
+import urllib.request as request
+import zipfile
+from textSummarizer.logging import logger
+from textSummarizer.utils.common import get_size
+from pathlib import Path
+from textSummarizer.config.configuration import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config:DataIngestionConfig):
+        self.config= config
+
+    def download_file(self):
+
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following inf: \n{headers}")
+
+        else:
+            logger.info(f"file already exists of size : {get_size(Path(self.config.local_data_file))}")
+
+    def extract_zip_file(self):
+        """
+        Extracting the zipfile into data directiory
+        """
+        unzip_path=self.config.unzip_dir
+
+        os.makedirs(unzip_path, exist_ok=True)
+
+        with zipfile.ZipFile(self.config.local_data_file,'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
