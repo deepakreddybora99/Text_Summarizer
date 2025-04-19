bfc96f3fc468efd0416cde58ec49af6374643748
deepakreddybora99 <deepakreddybora@gmail.com>
2025-04-19 17:06:45 +0530

    Data Transformation Added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -19,8 +19,19 @@ STAGE_NAME="Data Validation Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion=DataValidationTrainingPipeline()
-    data_ingestion.main()
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation=DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,9 @@
 #configuration
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+from textSummarizer.entity import (DataIngestionConfig, 
+                                   DataValidationConfig,
+                                   DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -28,6 +30,7 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+        
     def get_data_validation(self)->DataValidationConfig:
 
         config=self.config.data_validation
@@ -39,3 +42,16 @@ class ConfigurationManager:
             ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
         )
         return data_validation_config
+    
+    
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+        config=self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config= DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path= config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+        return data_transformation_config
